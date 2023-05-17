9856bd895c5bc3cc9e1dd40139a6368f9377efa8
WillReynolds5 <willmcleanreynolds@gmail.com>
2023-05-17 15:15:28 -0700

    edited setup

--- setup.py
+++ setup.py
@@ -6,12 +6,7 @@ setup(
     packages=find_packages(),
     description='A command-line application designed to extract structured data from financial news sources.',
     install_requires=[
-        # list all of your project's dependencies here
-        'requests',
-        'beautifulsoup4',
-        'openai',
-        # ...
-    ],
+        'aiohttp' 'aiosignal' 'async-timeout' 'attrs' 'beautifulsoup4' 'brotlipy' 'certifi' 'cffi' 'charset-normalizer' 'colorama' 'cryptography' 'dataclasses-json' 'frozenlist' 'greenlet' 'idna' 'langchain' 'marshmallow' 'marshmallow-enum' 'mkl-fft' 'mkl-random' 'mkl-service' 'multidict' 'mypy-extensions' 'numexpr' 'numpy' 'openai' 'openapi-schema-pydantic' 'packaging' 'pycparser' 'pydantic' 'pyOpenSSL' 'PySocks' 'python-dotenv' 'PyYAML' 'regex' 'requests' 'six' 'soupsieve' 'SQLAlchemy' 'stringcase' 'tenacity' 'tiktoken' 'tqdm' 'typing' 'typing-inspect' 'typing_extensions' 'urllib3' 'yarl'],
     entry_points={
         'console_scripts': [
             'sentimentx=sentimentx:main',  # this allows users to run your script with the `sentimentx` command
